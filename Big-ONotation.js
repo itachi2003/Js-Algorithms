8f22b69238684ede4ba15dfac3a70d4142d9691e
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-06-03 22:32:46 +0400

    first algorithm

---
+++ Big-ONotation.js
@@ -0,0 +1,17 @@
+// example 1
+function summation() {
+    let sum = 0;
+    for (let i = 1; i <= 4; i++) {
+        sum = i + sum; // 1 3 6 10
+    }
+    return sum;
+}
+console.log(summation());
+
+// example 2
+function summation() {
+    n = 1;
+    return (n * (n + 1)) /2 ;
+}
+console.log(summation());
+
