3f2912903eb424283931f59c3986b4fc9893b6ad
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-06 00:44:52 +0400

    Update QuickSort.js

--- QuickSort.js
+++ QuickSort.js
@@ -1,1 +1,25 @@
 
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else if (arr[i] > pivot) {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+// Example usage:
+const array = [5, 8, 2, 1, 6, 3, 7, 4];
+const sortedArray = quickSort(array);
+console.log(sortedArray);
