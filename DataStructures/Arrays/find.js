4f015add89f9cb2cf712d40635cf8928b6eece65
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-19 23:40:27 +0400

    Create find.js

---
+++ DataStructures/Arrays/find.js
@@ -0,0 +1,7 @@
+const numbers = [10, 25, 5, 40, 12];
+
+const foundNumber = numbers.find((number) => number > 20);
+
+console.log(foundNumber); // Output: 25
+
+// The find method returns the first element in the array that satisfies the provided testing function.
