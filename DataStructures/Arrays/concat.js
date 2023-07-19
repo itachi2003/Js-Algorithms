65861dfb07f93201754334c6147fddc2db1b6e25
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-19 23:46:18 +0400

    Create concat.js

---
+++ DataStructures/Arrays/concat.js
@@ -0,0 +1,9 @@
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+const arr3 = [5, 6];
+
+const mergedArray = arr1.concat(arr2, arr3);
+
+console.log(mergedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+// The concat method is used to merge two or more arrays, creating a new array.
