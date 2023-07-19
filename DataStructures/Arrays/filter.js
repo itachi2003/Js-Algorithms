1aaa3fa94af6e3c958319e2b42d9642d270b37d8
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-19 23:41:57 +0400

    Create filter.js

---
+++ DataStructures/Arrays/filter.js
@@ -0,0 +1,7 @@
+const numbers = [10, 5, 20, 15, 25];
+
+const filteredNumbers = numbers.filter((number) => number > 10);
+
+console.log(filteredNumbers); // Output: [20, 15, 25]
+
+// The filter method creates a new array with all elements that pass the provided test function.
