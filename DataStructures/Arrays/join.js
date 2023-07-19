d2805c251d279d4eea5e264505141399e267878a
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-19 23:45:34 +0400

    Create join.js

---
+++ DataStructures/Arrays/join.js
@@ -0,0 +1,8 @@
+const fruits = ["apple", "orange", "banana"];
+
+const joinedFruits = fruits.join("$ ");
+
+console.log(joinedFruits); // Output: "apple$ orange$ banana"
+
+
+// The join method creates a string by concatenating all elements of an array with a specified separator.
