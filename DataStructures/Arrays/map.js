c979b1d931a8f5d39c9f5f4e3de90728e3779b8b
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-19 23:41:15 +0400

    Create map.js

---
+++ DataStructures/Arrays/map.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3];
+
+const doubledNumbers = numbers.map((number) => number * 2);
+
+console.log(doubledNumbers); // Output: [2, 4, 6]
+
+// The map method creates a new array by applying a provided function to each element in the original array.
